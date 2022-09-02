0958ab01be0ad33c87db163c2254fc632d7e641f
张森云 <zhangsy@citydo.com.cn>
2022-09-02 16:56:58 +0800

    add end

--- src/router/index2.js
+++ src/router/index2.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+// secret 密钥
+const secretKey = 'itheima No2 haha';
+
 const router = express.Router();
 const {request} = require('express');
 const mysql = require('mysql');
+const expressJWT = require('express-jwt');
 const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
@@ -9,6 +14,7 @@ const db = mysql.createPool({
   database: 'village_manage',
 })
 
+router.use(expressJWT({secret: secretKey,algorithms:['HS256']}).unless({path: [/^\/api\//]})); // algorithms:['HS256'] 防止报错
 router.post('/api/login', (req,res) => {
   const user = {
     ...req.query,
@@ -19,21 +25,28 @@ router.post('/api/login', (req,res) => {
     if (results.length === 0) {
       return res.send({status:1, msg:'登陆失败！'});
     }
-    req.session.user = req.query;
-    req.session.isLogin = true;
-    res.send({status:0, msg:'登陆成功！'})
+    res.send({
+      status: 200,
+      msg:'登陆成功！',
+      token: jwt.sign({username:user.username},secretKey, {expiresIn: '30s'})
+    })
+  })
+})
+// 有权限的qpi接口
+router.get('/admin/getinfo',(req, res) => {
+  console.log(req.user);
+  res.send({
+    status: 200,
+    msg:'获取用户信息成功！',
+    data: req.user,
   })
 })
 // 获取用户姓名
 router.get('/api/user/username', (req,res) => {
-  if (!req.session.isLogin) {
-    return res.send({status: 1, msg:'fail'});
-  }
-  res.send({status: 0, msg: 'success', username: req.session.user.username})
+  res.send({status: 0, msg: 'success'})
 })
 // 退出登陆
 router.post('/api/logout', (req,res) => {
-    req.session.destroy();
     res.send({status:0, msg:'退出成功！'})
 })
 router.get('/api/user/:id', (req,res) => {
--- src/test_02.js
+++ src/test_02.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const session = require('express-session')
-const router = require('./router/index');
+const router = require('./router/index2');
 const path = require('path');
 const mysql = require('mysql');
+// const expressJWT = require('express-jwt');
+// const secretKey = 'itheima No2 haha';
 const app = express();
 const fpath = path.join(__dirname ,'./clock')
 const mw = (req, res, next) => {
@@ -13,17 +14,19 @@ const mw2 = (req, res, next) => {
   res.name2 = res.name + 100;
   next();
 }
-app.use(session({
-  secret:'keyboard cat',
-  resave: 'false',
-  saveUninitialized: true,
-}))
+// app.use(expressJWT({secret: secretKey,algorithms:['HS256']}).unless({path: [/^\/api\//]}));
 app.use(express.static(fpath));
 app.use(mw);
 app.use(mw2);
 app.use(router);
 app.use((err, req, res, next) => {
-  res.send('errr' + err.message);
+  if (err.name === 'UnauthorizedError') {
+    return res.send({status: 401, message: '无效的token'})
+  }
+  // 其他原因导致的错误
+  if (err) {
+    res.send({status: 500, message: '未知错误'})
+  }
 });
 app.listen('8082', () => {
   console.log('server running at http://127.0.0.1:8082')
